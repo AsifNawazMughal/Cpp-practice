c0066bbcca9632364649fb6d07e52158f64dbaee
Asif Nawaz <161076678+AsifNawazMughal@users.noreply.github.com>
2026-08-28 05:53:59 +0500

    while loop

---
+++ Loops/Add100Number.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std; 
+
+int main()
+{
+  // while     intial variable
+  //   controller 
+  int i = 0; 
+  while(i<=1000){
+    //print i; 
+    cout<<i<<endl;
+    i+=2;   // i+2
+  }
+}
